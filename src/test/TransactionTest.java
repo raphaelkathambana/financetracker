3b7887bd1096d797cfb1f1ff2d7b31706ad5caa0
Raphael Kathambana <maya12raph@gmail.com>
2023-06-19 22:18:36 +0300

    added basic tests

--- src/test/TransactionTest.java
+++ src/test/TransactionTest.java
@@ -1,5 +1,24 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import util.Category;
+import util.Transaction;
+import util.parentCategory;
+
 public class TransactionTest {
-    
+    @Test
+    public void testDisplayTransaction() {
+        String dateString = "2020-10-17";
+        var t = new Transaction("", dateString, 20, new Category("", "", parentCategory.INCOME));
+        assertEquals(t.getDate().toString(), dateString);
+    }
+
+    @Test
+    public void testDifferentDateTypes() {
+        var t = new Transaction("", "2020-10-17", 20, new Category("", "", parentCategory.EXPENSE));
+        assertNotEquals(t.getDate(), t.getSqlDate());
+    }
 }
