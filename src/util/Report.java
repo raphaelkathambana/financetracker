dfd0b1fced1efb13016b1fa00d5c94b3438d8f82
Raphael Kathambana <maya12raph@gmail.com>
2023-06-19 22:20:47 +0300

    changed date to localdate

--- src/util/Report.java
+++ src/util/Report.java
@@ -1,59 +1,50 @@
 package util;
 
-import java.util.Date;
 import java.util.List;
+import java.time.LocalDate;
 
 public class Report {
-private String reportType;
-private Date startDate;
-private Date endDate;
-private List<Transaction> transactions;
+    private String reportType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private List<Transaction> transactions;
 
+    public Report(String reportType, String startDateString, String endDateString, List<Transaction> transactions) {
+        this.reportType = reportType;
+        this.startDate = LocalDate.parse(startDateString);
+        this.endDate = LocalDate.parse(endDateString);
+        this.transactions = transactions;
+    }
 
-public Report(String reportType, Date startDate, Date endDate, List<Transaction> transactions) {
-    this.reportType = reportType;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.transactions = transactions;
-}
+    public String getReportType() {
+        return reportType;
+    }
 
+    public void setReportType(String reportType) {
+        this.reportType = reportType;
+    }
 
-public String getReportType() {
-    return reportType;
-}
+    public LocalDate getStartDate() {
+        return startDate;
+    }
 
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
 
-public void setReportType(String reportType) {
-    this.reportType = reportType;
-}
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 
-public Date getStartDate() {
-    return startDate;
-}
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
 
-
-public void setStartDate(Date startDate) {
-    this.startDate = startDate;
-}
-
-
-public Date getEndDate() {
-    return endDate;
-}
-
-
-public void setEndDate(Date endDate) {
-    this.endDate = endDate;
-}
-
-
-public List<Transaction> getTransactions() {
-    return transactions;
-}
-
-
-public void setTransactions(List<Transaction> transactions) {
-    this.transactions = transactions;
-}
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
