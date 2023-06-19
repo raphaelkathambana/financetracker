1498a9dc63157128b71cfcdb89b39b4cc55e8621
Raphael Kathambana <maya12raph@gmail.com>
2023-06-19 22:55:55 +0300

    updated category call in tests

--- src/test/TransactionTest.java
+++ src/test/TransactionTest.java
@@ -12,13 +12,13 @@ public class TransactionTest {
     @Test
     public void testDisplayTransaction() {
         String dateString = "2020-10-17";
-        var t = new Transaction("", dateString, 20, new Category("", "", parentCategory.INCOME));
+        var t = new Transaction("", dateString, 20, new Category("", "", parentCategory.INCOME.getCategory()));
         assertEquals(t.getDate().toString(), dateString);
     }
 
     @Test
     public void testDifferentDateTypes() {
-        var t = new Transaction("", "2020-10-17", 20, new Category("", "", parentCategory.EXPENSE));
+        var t = new Transaction("", "2020-10-17", 20, new Category("", "", parentCategory.EXPENSE.getCategory()));
         assertNotEquals(t.getDate(), t.getSqlDate());
     }
 }
