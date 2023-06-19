8522a397720fa8c6655282ec88614903fb567b26
Raphael Kathambana <maya12raph@gmail.com>
2023-06-19 22:21:17 +0300

    coded basic class functionality

--- src/util/Transaction.java
+++ src/util/Transaction.java
@@ -1,19 +1,30 @@
 package util;
 
-import java.util.Date;
-import java.util.Locale.Category;
+import java.time.LocalDate;
 
-public class Transaction {
-    private String type;
-    private Date date;
-    private double amount;
-    private Category category;
+public class Transaction { // Declare the Transaction class
+    private String type; // Declare Type variable
+    private Category category; // Declare category variable
+    private LocalDate date; // Declare date variable
+    private double amount; // Declare amount variable
 
-    public Transaction(String type, Date date, double amount, Category category) {
+    public Transaction(String type, String dateString, double amount, Category category) {
         this.type = type;
-        this.date = date;
+        this.date = LocalDate.parse(dateString);
         this.amount = amount;
         this.category = category;
+    }
+
+    public void displayInfo() { // Declare displayInfo method
+        System.out.println("Type: " + this.type + ", Date: " + this.date + ", Amount: " + this.amount + ", Category: "
+                + this.category); // Print details of transaction
+    }
+
+    public java.sql.Date getSqlDate() {
+        return java.sql.Date.valueOf(this.date);
+    }
+    public void saveToDB() { // Declare saveToDB method
+        // code to save to mysql database // Function body
     }
 
     public String getType() {
@@ -24,11 +35,11 @@ public class Transaction {
         this.type = type;
     }
 
-    public Date getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
