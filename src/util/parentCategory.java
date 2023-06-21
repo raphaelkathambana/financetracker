3120d9ddd9ab3f1b8df45f30e7026605462a9982
Raphael Kathambana <maya12raph@gmail.com>
2023-06-21 15:34:01 +0300

    edited expense parent category

--- src/util/parentCategory.java
+++ src/util/parentCategory.java
@@ -3,7 +3,7 @@ package util;
 public enum parentCategory {
 
     INCOME(new Category("Income", "All sources of income", null)),
-    EXPENSE(new Category("Expenses", "All types of expenses"));
+    EXPENSE(new Category("Expenses", "All types of expenses", null));
 
     private final Category category;
 
