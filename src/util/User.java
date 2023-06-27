61b8d11c82a48e412583c0fb21a9fade6f7606fa
Raphael Kathambana <maya12raph@gmail.com>
2023-06-27 22:42:01 +0300

    added expense and income methods

--- src/util/User.java
+++ src/util/User.java
@@ -1,44 +1,78 @@
 package util;
 
+import java.sql.*;
 public class User {
-private String name;
-private String email;
-private String password;
+    private String name;
+    private String email;
+    private String password;
 
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-public User(String name, String email, String password) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-}
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public String getName() {
-    return name;
-}
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-public void setName(String name) {
-    this.name = name;
-}
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-public String getEmail() {
-    return email;
-}
+    public static void addIncomeTransaction(int userId, double amount, String category, String date) {
+        try (Connection connection = GetConnection.getConn();
+                PreparedStatement statement = connection.prepareStatement(
+                        "INSERT INTO Income (user_id, amount, category, date) VALUES (?, ?, ?, ?);");) {
+            statement.setInt(1, userId);
+            statement.setDouble(2, amount);
+            statement.setString(3, category);
+            statement.setString(4, date);
+            statement.executeUpdate();
+            System.out.println("Income transaction added successfully.");
+        } catch (SQLException e) {
+            System.out.println("Error occurred while adding the income transaction: " + e.getMessage());
+        }
+    }
 
+    // Retrieve expenses for a specific user from the database
+    public static void getExpensesForUser(int userId) {
+        try (Connection connection = GetConnection.getConn();
+                PreparedStatement statement = connection.prepareStatement(
+                        "SELECT * FROM Expense WHERE user_id = ?;");) {
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
 
-public void setEmail(String email) {
-    this.email = email;
-}
+            while (resultSet.next()) {
+                int expenseId = resultSet.getInt("expense_id");
+                double amount = resultSet.getDouble("amount");
+                String category = resultSet.getString("category");
+                String date = resultSet.getString("date");
 
-
-public String getPassword() {
-    return password;
-}
-
-
-public void setPassword(String password) {
-    this.password = password;
-}
+                System.out.println("Expense ID: " + expenseId);
+                System.out.println("Amount: " + amount);
+                System.out.println("Category: " + category);
+                System.out.println("Date: " + date);
+                System.out.println("--------------------");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error occurred while retrieving expenses: " + e.getMessage());
+        }
+    }
 }
