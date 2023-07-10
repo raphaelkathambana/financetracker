7287e39d43119615d57febda35756081fa0f4b54
Raphael Kathambana <maya12raph@gmail.com>
2023-07-10 11:59:29 +0300

    updated connection Test

--- src/test/SqlTest.java
+++ src/test/SqlTest.java
@@ -1,31 +1,37 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import util.GetConnection;
-
 public class SqlTest {
     private static String name;
+    private static String password;
+    private static String url;
+
     @BeforeClass
     public static void loadKey() throws IOException {
         Properties properties = new Properties();
         properties.load(SqlTest.class.getResourceAsStream("test.properties"));
         name = properties.getProperty("username");
-    }
-    @Test
-    public void test() {
-        assertEquals("root", name); 
+        password = properties.getProperty("password");
+        url = properties.getProperty("url");
     }
 
     @Test
-    public void testConnection() {
-        assertNull(GetConnection.getConn());
+    public void testSuccessfulLoadFromProperties() {
+        assertEquals("root", name);
+    }
+
+    @Test
+    public void testDbConnection() throws SQLException {
+        assertNotNull(DriverManager.getConnection(url, name, password));
     }
 }
